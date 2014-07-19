40728545b24a4ddd5511211b0e8018f12eb01a7c
BluePick <user@Xoom3-Mac.local>
2014-07-19 16:43:57 +0530

    001

--- CustomFont/CustomFontTextView.h
+++ CustomFont/CustomFontTextView.h
@@ -1,6 +1,14 @@
+//
+//  CustomFontTextView.h
+//  Poetry
+//
+//  Created by xoom3 on 5/28/14.
+//  Copyright (c) 2014 XOOM. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
+#import "MPTextView.h"
 
-@interface CustomFontTextView : UITextView
+@interface CustomFontTextView : MPTextView
 
 @end
--- CustomFont/CustomFontTextView.m
+++ CustomFont/CustomFontTextView.m
@@ -1,4 +1,10 @@
-
+//
+//  CustomFontTextView.m
+//  Poetry
+//
+//  Created by xoom3 on 5/28/14.
+//  Copyright (c) 2014 XOOM. All rights reserved.
+//
 
 #import "CustomFontTextView.h"
 
@@ -6,7 +12,13 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    self.font = [UIFont fontWithName:self.tag?@"Whipsmart":@"Whipsmart-Bold" size:self.font.pointSize];
+    self.font = [UIFont fontWithName:@"Quadon" size:self.font.pointSize];
+    CALayer *layer = [self layer];
+    [layer setBorderColor:COLOR_BG.CGColor];
+    [layer setBorderWidth:2];
+    self.placeholderText = self.accessibilityHint;
+    self.text = self.text;
+    
 }
 
 @end
